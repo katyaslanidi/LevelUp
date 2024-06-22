a7907f2be1a26af98019af6ecbcb0b88c16d9146
Ekaterina Aslanidi <ekaterinaaslanidi@MacBook-Air-Ekaterina.local>
2024-06-22 07:01:43 +0300

    test

---
+++ timer.js
@@ -0,0 +1,12 @@
+let time = 1800;
+const countDownEl = document.getElementById("countdown");
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds: seconds;
+    countDownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
